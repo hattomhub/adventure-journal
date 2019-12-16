9c38b328000430bb18cf4051a4054fe9da0755a2
carcaso <ruisilro@gmail.com>
2019-12-16 18:39:59 +0000

    added user in model

---
+++ backend/src/main/java/vigaristas/adventurejournal/persistence/model/User.java
@@ -0,0 +1,51 @@
+package vigaristas.adventurejournal.persistence.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "user")
+public class User extends AbstractModel {
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "email", unique = true)
+    @Email
+    private String email;
+    private String md5passwd;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMd5passwd() {
+        return md5passwd;
+    }
+
+    public void setMd5passwd(String md5passwd) {
+        this.md5passwd = md5passwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
